fe256fe2646813a033bf2b49ba5f1d48a7b9406f
Marc Hershey <marchershey@users.noreply.github.com>
2024-01-24 06:11:38 -0500

    wip

---
+++ src/app/Http/Pages/Setup/Steps/RentalPhotos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Pages\Setup\Steps;
+
+use Livewire\Component;
+
+class RentalPhotos extends Component
+{
+    public function render()
+    {
+        return view('pages.setup.steps.rental-photos');
+    }
+}
