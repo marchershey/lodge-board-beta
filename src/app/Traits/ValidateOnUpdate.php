8c5982d730c2a2369eea6d0e4fafaa84f375ae85
Marc Hershey <marchershey@users.noreply.github.com>
2024-05-09 22:34:43 -0400

    wip

---
+++ src/app/Traits/ValidateOnUpdate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Traits;
+
+use Livewire\Component;
+
+trait ValidateOnUpdate
+{
+    /**
+     * Runs when a component has been updated/changed only when value is not null.
+     * 
+     * After validation, if a property is invalid then the user updates the property,
+     * reset the property's validation, but do not rerun validation until the user
+     * resubmits the form
+     *
+     * @param string $property
+     * @param string $value
+     * @return void
+     */
+    function updated($property, $value): void
+    {
+        $this->resetValidation($property);
+        // This validates the property, ONLY if it isn't null. 
+        if ($value) {
+            $this->validateOnly($property);
+        }
+
+        $this->validateOnly($property);
+    }
+}
