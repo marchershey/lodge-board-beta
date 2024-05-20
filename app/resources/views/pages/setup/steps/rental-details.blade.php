5a60aaf3e39fbaefc32245dcc77e906a20e24745
Marc Hershey <marchershey@users.noreply.github.com>
2024-05-19 22:22:29 -0400

    wip

--- app/resources/views/pages/setup/steps/rental-details.blade.php
+++ app/resources/views/pages/setup/steps/rental-details.blade.php
@@ -1,3 +1,9 @@
 <section class="card card-padding card-flex" wire:init="load">
     <p>Great! We need a few more details about {{ $rental->name ?? '...' }}.</p>
+
+    <form class="form-grid" wire:submit.prevent="submit">
+        <x-forms.text wiremodel="rental_name" label="Rental Name" desc="Give your rental property a name." />
+        <x-forms.text class="capitalize" type="text" wiremodel="rental_street" label="Street Address" placeholder="Street Address" desc="This needs to be the actual address, if you need to give your guests a different address, that can be done later." />
+        <x-forms.text class="capitalize" type="text" wiremodel="rental_city" label="City" placeholder="City" />
+    </form>
 </section>
